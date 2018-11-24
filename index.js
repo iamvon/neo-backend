ec7e27be43a04e6cededa8204d54960496710ec5
tuanpmhd <tuanpmhd@gmail.com>
2018-11-24 22:08:00 +0200

    export module nao bro

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ let contractAddr = '0x87a61ee8b47876e6ef437c4c403687d2a4f28804'
 let WIF = 'KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr'
 let account = Neon.create.account(WIF);
 let privateKey = Neo.wallet.getPrivateKeyFromWIF(WIF)
-const address = require('./properties').address
-let networkUrl = `http://${address}:30333`  //'http://localhost:30333'
-// console.log(networkUrl)
-let neoscanUrl = `http://${address}:4000/api/main_net` //'http://localhost:4000/api/main_net'
+const localAddress = require('./properties').address
+let networkUrl = `http://${localAddress}:30333`  //'http://localhost:30333'
+let neoscanUrl = `http://${localAddress}:4000/api/main_net` //'http://localhost:4000/api/main_net'
 let addr_1 = "e9eed8dc39332032dc22e5d6e86332c50327ba23"
 const scriptHash = '1825a77f40149e0272b217c480b676136a2f456f' // Scripthash for the contract
 
@@ -37,42 +36,96 @@ const scriptHash = '1825a77f40149e0272b217c480b676136a2f456f' // Scripthash for 
 
 //===========================================================================================
 
-// List of smart contract method scripts
-let amountToken = 50;
-const setToken = Neon.create.script({ scriptHash: scriptHash, operation: "setToken", args: [Neon.u.reverseHex(addr_1), amountToken]})
-const bonusToken = Neon.create.script({ scriptHash: scriptHash, operation: "bonusToken", args: [Neon.u.reverseHex(addr_1), amountToken]})
-const minusToken = Neon.create.script({ scriptHash: scriptHash, operation: "minusToken", args: [Neon.u.reverseHex(addr_1), amountToken]})
-const get_BalanceOf = {scriptHash, operation: 'balanceOf', args: [Neon.u.reverseHex(addr_1)]}
-const get_getTime = {scriptHash, operation: 'getTime', args: []}
+const writeRequest = {
+  net: neoscanUrl,
+  url: networkUrl,
+  script: null,  
+  address: null,
+  privateKey: account.privateKey,
+  publicKey: account.publicKey,
+  gas: 0,
+  balance: null
+};
 
-// writeRequest to choose what method to write to NEO blockchain
-// const writeRequest = {
-//   net: neoscanUrl,
-//   url: networkUrl,
-//   script: minusToken,  // Change this method script to WRITE
-//   address: account.address,
-//   privateKey: account.privateKey,
-//   publicKey: account.publicKey,
-//   gas: 0,
-//   balance: null
-// };
+let setToken = function(address, amount) {
+  setToken = Neon.create.script({ scriptHash: scriptHash, operation: "setToken", args: [Neon.u.reverseHex(Neo.wallet.getScriptHashFromAddress(address)), amount]})
 
-// Sign and writing to NEO blockchain
-// Neo.api.neoscan.getBalance(neoscanUrl, account.address).then(data => {
-//   writeRequest.balance = data;
-//   Neon.doInvoke(writeRequest).then(res => console.log(res.response));
-// }).catch(err => {
-//   console.log(err)
-// });
+  Neo.api.neoscan.getBalance(neoscanUrl, account.address).then(data => {
+    let setTokenRequest = writeRequest
+    setTokenRequest.address = address
+    setTokenRequest.script = setToken
+    setTokenRequest.balance = data
+    Neon.doInvoke(setTokenRequest).then(res => console.log(res.response));
+  }).catch(err => {
+    console.log(err)
+  });
+}
 
+let bonusToken = function(address, amount) {
+  bonusToken = Neon.create.script({ scriptHash: scriptHash, operation: "bonusToken", args: [Neon.u.reverseHex(Neo.wallet.getScriptHashFromAddress(address)), amount]})
 
-// Query the NEO blockchain
-const multipleQueryScript = Neon.create.script(get_getTime)  // Change this method script to READ
-rpc.Query.invokeScript(multipleQueryScript)
+  Neo.api.neoscan.getBalance(neoscanUrl, account.address).then(data => {
+    let bonusTokenRequest = writeRequest
+    bonusTokenRequest.address = address
+    bonusTokenRequest.script = bonusToken
+    bonusTokenRequest.balance = data
+    Neon.doInvoke(bonusTokenRequest).then(res => console.log(res.response));
+  }).catch(err => {
+    console.log(err)
+  });
+}
+
+let minusToken = function(address, amount) {
+  minusToken = Neon.create.script({ scriptHash: scriptHash, operation: "minusToken", args: [Neon.u.reverseHex(Neo.wallet.getScriptHashFromAddress(address)), amount]})
+
+  Neo.api.neoscan.getBalance(neoscanUrl, account.address).then(data => {
+    let minusTokenRequest = writeRequest
+    minusTokenRequest.address = address
+    minusTokenRequest.script = minusToken
+    minusTokenRequest.balance = data
+    Neon.doInvoke(minusTokenRequest).then(res => console.log(res.response));
+  }).catch(err => {
+    console.log(err)
+  });
+}
+
+let balanceOf = function(address) {
+  get_BalanceOf = {scriptHash, operation: 'balanceOf', args: [Neon.u.reverseHex(Neo.wallet.getScriptHashFromAddress(address))]}
+
+  const balanceOfScript = Neon.create.script(get_BalanceOf) 
+  rpc.Query.invokeScript(balanceOfScript)
   .execute(networkUrl)                 
   .then(res => {
     console.log("Balance of:", addr_1, ":",res.result.stack[0].value) 
 }).catch(error => {
   console.log(error)
 })
+}
 
+let getTime = function() {
+  getTime = {scriptHash, operation: 'getTime', args: []}
+
+  const getTimeScript = Neon.create.script(getTime) 
+  rpc.Query.invokeScript(getTimeScript)
+  .execute(networkUrl)                 
+  .then(res => {
+    console.log("unixTime:", res.result.stack[0].value) 
+}).catch(error => {
+  console.log(error)
+})
+}
+
+// setToken(account.address, 500)
+// bonusToken(account.address, 100)
+// minusToken(account.address, 50)
+// balanceOf(account.address)
+// getTime()
+
+module.exports = {
+  setToken, 
+  bonusToken, 
+  minusToken, 
+  balanceOf, 
+  getTime,
+  account
+}
